1e4c7ee0e4e3201d32dc9d93638c629f000ce5ac
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-26 22:12:24 +0800

    "返回结果生成"

--- frameworks/web/src/main/java/com/example/Results.java
+++ frameworks/web/src/main/java/com/example/Results.java
@@ -7,18 +7,16 @@ import com.example.result.Result;
  */
 public class Results {
     public static Result<Void> success() {
-        return new Result<Void>().setCode(Result.SUCCESS_CODE);
-    }
-
-    public static <T> Result<T> success(T data) {
-        return new Result<T>().setData(data)
+        return new Result<Void>()
                 .setCode(Result.SUCCESS_CODE);
     }
 
-    public static Result<Void> failure() {
-        return new Result<Void>()
-                .setCode("");
+    public static <T> Result<T> success(T data) {
+        return new Result<T>()
+                .setData(data)
+                .setCode(Result.SUCCESS_CODE);
     }
 
+    //TODO 返回失败类
 
 }
