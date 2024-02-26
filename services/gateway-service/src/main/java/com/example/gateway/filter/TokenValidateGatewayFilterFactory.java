03a536674162c3d35f357bba5843730c2377b32e
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-26 20:36:07 +0800

    "自定义Token拦截"

--- services/gateway-service/src/main/java/com/example/gateway/filter/TokenValidateGatewayFilterFactory.java
+++ services/gateway-service/src/main/java/com/example/gateway/filter/TokenValidateGatewayFilterFactory.java
@@ -1,12 +1,26 @@
 package com.example.gateway.filter;
 
+import com.example.constant.UserConstant;
 import com.example.gateway.config.Config;
+import com.example.gateway.util.JWTUtil;
+import com.example.gateway.util.UserInfoDTO;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 自定义SCG 拦截器，进行Token验证
+ */
 @Component
 public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFactory<Config> {
 
@@ -14,12 +28,58 @@ public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFact
         super(Config.class);
     }
 
+    public static final String DELETION_PATH = "/api/user-service/deletion";
+
+
+    /**
+     * 对重要接口进行Token验证
+     * 解析Token，在Header添加身份信息
+     * @param config
+     * @return
+     */
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            System.out.println("hi,gateway");
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
+            ServerHttpRequest request = exchange.getRequest();
+            String requestPath = request.getPath().toString();
+            if (isPathInBlackPreList(requestPath, config.getBlackPathPre())) {
+                // 获取 Token
+                String token = request.getHeaders().getFirst("Authorization");
+                UserInfoDTO userInfo = JWTUtil.parseJwtToken(token);
+                if (!validateToken(userInfo)) {
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return response.setComplete();
+                }
+
+                ServerHttpRequest.Builder builder = exchange.getRequest().mutate().headers(httpHeaders -> {
+                    httpHeaders.set(UserConstant.USER_ID_KEY, userInfo.getUserId());
+                    httpHeaders.set(UserConstant.USER_NAME_KEY, userInfo.getUsername());
+                    httpHeaders.set(UserConstant.REAL_NAME_KEY, URLEncoder.encode(userInfo.getRealName(), StandardCharsets.UTF_8));
+                    if (Objects.equals(requestPath, DELETION_PATH)) {
+                        httpHeaders.set(UserConstant.USER_TOKEN_KEY, token);
+                    }
+                });
+                return chain.filter(exchange.mutate().request(builder.build()).build());
+            }
+            return chain.filter(exchange);
         };
+    }
+
+    /**
+     * 列表验证
+     * @param requestPath
+     * @param blackPathPre
+     * @return
+     */
+    private boolean isPathInBlackPreList(String requestPath, List<String> blackPathPre) {
+        if (CollectionUtils.isEmpty(blackPathPre)) {
+            return false;
+        }
+        return blackPathPre.stream().anyMatch(requestPath::startsWith);
+    }
+
+    private boolean validateToken(UserInfoDTO userInfo) {
+        return userInfo != null;
     }
 }
