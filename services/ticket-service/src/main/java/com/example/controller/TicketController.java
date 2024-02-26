f6213d75f61388cfa8990567852de00afe89aa52
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-26 20:09:53 +0800

    "用户信息DTO"

--- services/ticket-service/src/main/java/com/example/controller/TicketController.java
+++ services/ticket-service/src/main/java/com/example/controller/TicketController.java
@@ -1,33 +1,35 @@
 package com.example.controller;
 
+import com.example.Results;
 import com.example.dto.req.TicketPageQueryReqDTO;
 import com.example.result.Result;
 import com.example.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class TicketController {
 
     @Autowired
     private TicketService ticketService;
 
+    /**
+     * 车票查询，使用Redis + MySQL实现
+     * @param requestParam
+     * @return
+     */
     @GetMapping("/api/ticket-service/ticket/query")
     public Result<Object> pageListTicketQuery(TicketPageQueryReqDTO requestParam) {
         return new Result<>().setData(ticketService.pageListTicketQueryV1(requestParam));
     }
+
+    public Result<String> purchaseTicket() {
+        return Results.success(ticketService.purchaseTicketsV1());
+    }
+
 
 }
