a26c85e5ea587446b136aed2f3434d0a300a41a8
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-26 20:36:48 +0800

    "访问黑名单"

--- services/gateway-service/src/main/java/com/example/gateway/config/Config.java
+++ services/gateway-service/src/main/java/com/example/gateway/config/Config.java
@@ -1,4 +1,14 @@
 package com.example.gateway.config;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Config {
+
+    /**
+     * 黑名单前置路径
+     */
+    private List<String> blackPathPre;
 }
