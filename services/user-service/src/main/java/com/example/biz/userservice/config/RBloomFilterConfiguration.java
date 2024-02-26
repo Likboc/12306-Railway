1c509161ab68a90a88b51d27e7dc5a58e9341dc4
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-26 21:55:31 +0800

    "Bloom Filter"

---
+++ services/user-service/src/main/java/com/example/biz/userservice/config/RBloomFilterConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.biz.userservice.config;
+
+import org.checkerframework.checker.units.qual.C;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册缓存穿透的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient, UserRegisterBloomFilterProperties userRegisterBloomFilterProperties) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter(userRegisterBloomFilterProperties.getName());
+        cachePenetrationBloomFilter.tryInit(userRegisterBloomFilterProperties.getExpectedInsertions(), userRegisterBloomFilterProperties.getFalseProbability());
+        return cachePenetrationBloomFilter;
+    }
+}
