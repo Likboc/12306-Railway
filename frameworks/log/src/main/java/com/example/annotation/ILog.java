e3036ef2b6b29be9f3f61ab6d34058013df5914d
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-26 23:39:18 +0800

    "Log AOP annotation"

---
+++ frameworks/log/src/main/java/com/example/annotation/ILog.java
@@ -0,0 +1,14 @@
+package com.example.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ILog {
+    boolean input() default true;
+
+    boolean output() default true;
+}
