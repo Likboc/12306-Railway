63a1bf1d202e0e19627ca2a9dbeab7aeb6945d88
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-29 14:24:44 +0800

    "过滤器注册"

---
+++ frameworks/biz/user/src/main/java/com/example/config/UserAutoConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.config;
+
+import com.example.core.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import static com.example.constant.FilterOrderConstant.USER_TRANSMIT_FILTER_ORDER;
+
+@ConditionalOnWebApplication
+public class UserAutoConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(USER_TRANSMIT_FILTER_ORDER);
+        return registration;
+    }
+}
