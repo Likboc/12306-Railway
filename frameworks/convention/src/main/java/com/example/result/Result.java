fc37fe2a76ba71feee81e43fc014e1b3b5db0b4b
downloader <git@github.com:Likboc/min-downloader.git>
2024-04-07 00:17:08 +0800

    "结果封装类"

--- frameworks/convention/src/main/java/com/example/result/Result.java
+++ frameworks/convention/src/main/java/com/example/result/Result.java
@@ -3,11 +3,16 @@ package com.example.result;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serial;
 import java.util.Objects;
 
 @Data
 @Accessors(chain = true)
 public class Result<T> {
+
+    @Serial
+    private static final long serialVersionUID = 5679018624309023727L;
+
     private T Data;
 
     public static String SUCCESS_CODE = "0";
@@ -19,7 +24,6 @@ public class Result<T> {
     private String message;
 
     private String requestId;
-
     public boolean isSucceed() {
         return Objects.equals(code, SUCCESS_CODE);
     }
